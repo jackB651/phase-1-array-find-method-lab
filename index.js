939192407b210fd323def394847a01d9360b3afc
Jonathon <jwbialas@gmail.com>
2022-07-14 15:19:19 -0500

    commit tree 235835da7eec622b60f814f6897ed1eb30ab2193

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function superbowlWin(array){
+    if(array.find(didWin) === true){
+    return array.year
+}
+}
+function didWin(element){
+   if(element.result === "W"){
+    return true
+   }else{
+    return undefined
+   }
+
+}
